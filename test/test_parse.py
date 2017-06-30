b88ec7dab27d3f9677749487fe35f1d4d5a223f5
utam0k <k0ma@utam0k.jp>
2017-06-30 20:48:38 +0900

    Change file structure.

---
+++ test/test_parse.py
@@ -0,0 +1,18 @@
+import unittest
+
+from nose.tools import eq_
+
+from client.parse import parse
+
+
+class ParseTest(unittest.TestCase):
+
+    def test_parse(self):
+        result = parse('./client/tests/data/example.xml')
+        expect = [[{'duration': 2, 'pitch': {'octave': 4, 'step': 'G'}}],
+                  [{'duration': 3, 'pitch': {'octave': 4, 'step': 'F'}},
+                   {'duration': 1, 'pitch': {'octave': 4, 'step': 'E'}},
+                   {'duration': 2, 'pitch': {'octave': 4, 'step': 'E'}},
+                   {'duration': 1, 'pitch': {'octave': 4, 'step': 'B'}},
+                   {'duration': 1, 'pitch': {'octave': 4, 'step': 'G'}}]]
+        eq_(result, expect)
